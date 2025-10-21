f77081ba83b6a6902ddaf422b4beaddd9fcb52d9
Rocco698 <698rocco@gmail.com>
2025-10-21 17:15:25 -0400

    Add files via upload

--- run_sim.py
+++ run_sim.py
@@ -39,91 +39,31 @@ mat_list.cross_sections = "/Users/rocco698/Desktop/Undergrad/Fall Semester - 202
 #       GEOMETRY DEFINITION
 # ################################################
 
-# Plane Equation Calculator
-def plane_from_points(p1, p2, p3, **kwargs):
-    p1, p2, p3 = map(np.array, (p1, p2, p3))
-    v1, v2 = p2 - p1, p3 - p1
-    normal = np.cross(v1,v2)
-    A, B, C = normal
-    D = np.dot(normal, p1)
-    return openmc.Plane(a=A, b=B, c=C, d=D, **kwargs)
+# outer / inner edge length (length of one hex side) in cm
+edge_outer = 1.0
+edge_inner = 0.6
 
+# create composite hexagonal prism surfaces
+hex_outer = openmc.model.HexagonalPrism(edge_length=edge_outer, orientation='y',
+                           origin=(0.0, 0.0), boundary_type='reflective')
+hex_inner = openmc.model.HexagonalPrism(edge_length=edge_inner, orientation='y',
+                           origin=(0.0, 0.0), boundary_type='reflective')
 
-fuel_out = openmc.ZCylinder(r=0.9)
-annularclad_in = openmc.ZCylinder(r=0.15)
-annularclad_out = openmc.ZCylinder(r=0.25)
-clad1 = openmc.YPlane(1, boundary_type='reflective')
-clad2 = openmc.YPlane(1.05, boundary_type='reflective')
-clad3 = plane_from_points(
-    [0.58,1,0],
-    [1.15,0,0],
-    [1.15,0,1],
-    boundary_type='reflective'
-)
-clad4 = plane_from_points(
-    [0.61,1.05,0],
-    [1.21,0,0],
-    [1.21,0,1],
-    boundary_type='reflective'
-)
-clad5 = plane_from_points(
-    [1.15,0,0],
-    [0.58,-1,0],
-    [0.58,-1,1],
-    boundary_type='reflective'
-)
-clad6 = plane_from_points(
-    [1.21,0,0],
-    [0.61,-1.05,0],
-    [0.61,-1.05,1],
-    boundary_type='reflective'
-)
-clad7 = openmc.YPlane(-1, boundary_type='reflective')
+# get interior regions (unary - gives interior region)
+region_outer = -hex_outer    # interior of outer hex
+region_inner = -hex_inner    # interior of inner hex
 
-clad8 = openmc.YPlane(-1.05, boundary_type='reflective')
+# Z bounds for prism (height = 1.0)
+z_min = openmc.ZPlane(z0=0.0, boundary_type='reflective')
+z_max = openmc.ZPlane(z0=1.0, boundary_type='reflective')
 
-clad9 = plane_from_points(
-    [-0.58,-1,0],
-    [-1.15,0,0],
-    [-1.25,0,1],
-    boundary_type='reflective'
-)
-clad10 = plane_from_points(
-    [-0.61,1.05,0],
-    [-1.21,0,0],
-    [-1.21,0,1],
-    boundary_type='reflective'
-)
-clad11 = plane_from_points(
-    [-1.15,0,0],
-    [-0.58,1,0],
-    [-0.58,1,1],
-    boundary_type='reflective'
-)
-clad12 = plane_from_points(
-    [-1.21,0,0],
-    [-0.61,1.05,0],
-    [-0.61,1.05,1],
-    boundary_type='reflective'
-)
+# hollow hex region = outer interior MINUS inner interior, extruded in Z
+hollow_hex_region = region_outer & ~region_inner & +z_min & -z_max
 
+# create a cell (no material needed if only visualizing)
+hollow_cell = openmc.Cell(name='hollow_hex', fill=moderator, region=hollow_hex_region)
 
-
-moderator_center = -annularclad_in
-annular_cladding = +annularclad_in & -annularclad_out
-region_fuel_in = +annularclad_out & -fuel_out
-region_gap = +fuel_out & -clad1 & -clad3 & -clad5 & -clad7 & -clad9 & clad11
-region_cladding = +clad1 & + clad3 & +clad5 & +clad7 & +clad9 & +clad11 & -clad2 & -clad4 & -clad6 & -clad8 & -clad10 & -clad12
-region_moderator = +clad2 & +clad4 & +clad6 & +clad8 & +clad10 & +clad12
-
-moderator_cell_1 = openmc.Cell(name='moderator', fill=moderator, region=moderator_center)
-annular_cladding_cell = openmc.Cell(name='Cladding', fill=clad, region=annular_cladding)
-fuel_cell = openmc.Cell(name='fuel', fill=fuel, region=region_fuel_in)
-gap_cell = openmc.Cell(name='Gap', fill=moderator, region=region_gap)
-cladding_outer_cell = openmc.Cell(name='Cladding Outer', fill=clad, region=region_cladding)
-moderator_out_cell = openmc.Cell(name='Moderator Outer', fill=moderator, region=region_moderator)
-
-geometry = openmc.Geometry([moderator_cell_1, annular_cladding_cell, fuel_cell, gap_cell, cladding_outer_cell, moderator_out_cell])
+geometry = openmc.Geometry([hollow_cell])
 geometry.export_to_xml()
 
 print(geometry)
@@ -145,7 +85,7 @@ print(settings)
 # ################################
 #  Plots Definition
 # ################################
-ww = 4
+ww = 1
 plot1 = openmc.Plot()
 plot1.width = (ww,ww)
 plot1.basis = 'xy'
